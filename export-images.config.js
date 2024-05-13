6cab979c72701cbc7ea612a1cc3c4ac9a7ac959e
Rafał Safin <rafal.safin@rafsaf.pl>
2024-05-13 21:44:22 +0200

    Use picture with avif format
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

--- export-images.config.js
+++ export-images.config.js
@@ -2,7 +2,7 @@
  * @type {import('next-export-optimize-images').Config}
  */
 const config = {
-    generateFormats: ['avif', 'webp'],
-  }
-  
-  module.exports = config
+  generateFormats: ["avif", "webp"],
+};
+
+module.exports = config;
